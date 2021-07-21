72105b4c3c91b19a2dc49312c1edbd3d1ecd3330
Princy4589 <princyandrianantenaina@yahoo.com>
2021-07-21 17:57:28 +0300

    first commit

---
+++ Complexe.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace nombre_complexe
+{
+    class Complexe
+    {
+       private int reel; //nombre reel
+       private int image; //nombre imaginaire
+       public int Reel{get=>reel;set=>reel=value;}
+       public int Image{get=>image;set=>image=value;}   
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace nombre_complexe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complexe c1=new Complexe(){
+                Reel=1,
+                Image=2
+            };
+            Console.WriteLine("c1= ");
+        }
+    }
+}
