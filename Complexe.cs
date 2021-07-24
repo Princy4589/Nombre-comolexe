89cc2b85781076580ea639365dab7b53707fec83
Princy4589 <princyandrianantenaina@yahoo.com>
2021-07-24 15:01:12 +0300

    projet csharp

--- Complexe.cs
+++ Complexe.cs
@@ -4,10 +4,37 @@ namespace nombre_complexe
 {
     class Complexe
     {
-       private int reel; //nombre reel
-       private int image; //nombre imaginaire
-       public int Reel{get=>reel;set=>reel=value;}
-       public int Image{get=>image;set=>image=value;}   
+     //le nombre reel
+       public double Reel{get;set;}
+       //le nombre imaginaire i
+       public double Image{get;set;}   
+        //constructeur
+       public Complexe(double reel=0,double image=0){
+           Reel=reel;
+           Image=image;
+       } 
+       //affichage
+      public void afficher(){
+           Console.WriteLine("c= "+Reel+" +"+Image+"i ");
+       }
+       public double Module(){
+           return Math.Sqrt(Reel*Reel+Image*Image);
+       }
+       //somme de 2 nombres complexes
+       public Complexe somme(Complexe c){
+           Complexe plus=new Complexe();
+           plus.Reel=Reel+c.Reel;
+           plus.Image=Image+c.Image;
+           return plus; 
+       }
+       //produit de 2 nombres complexes
+       public Complexe produit(Complexe c){
+           Complexe fois=new Complexe();
+           fois.Reel=(Reel*c.Reel-Image*c.Image);
+           fois.Image=(Reel*c.Image+Image*c.Reel);
+           return fois;
+       }
+      
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,11 +6,18 @@ namespace nombre_complexe
     {
         static void Main(string[] args)
         {
-            Complexe c1=new Complexe(){
-                Reel=1,
-                Image=2
-            };
-            Console.WriteLine("c1= ");
+            Complexe c1=new Complexe(1,1);                      
+            Complexe c2=new Complexe(2,3);       
+            Complexe c3=c1.somme(c2);
+            Complexe c4=c1.produit(c2);
+            c1.afficher();
+            c2.afficher();
+            c3.afficher();
+            c4.afficher();          
+            Console.WriteLine(c1.Module());
+            Console.ReadKey();
+            
+
         }
     }
 }
